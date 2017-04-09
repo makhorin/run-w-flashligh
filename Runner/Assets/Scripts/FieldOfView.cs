5efe4d82920697323472d06b7226b7612917acb2
a.mahorin <a.mahorin@crazypanda.ru>
2017-04-09 16:14:52 +0300

    init

---
+++ Runner/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FieldOfView : MonoBehaviour {
+
+	public float viewRadius;
+	[Range(0,360)]
+	public float viewAngle;
+
+	public LayerMask targetMask;
+
+	[HideInInspector]
+	public List<Transform> visibleTargets = new List<Transform>();
+
+	public float meshResolution;
+
+	public float maskCutawayDst = .1f;
+
+	public MeshFilter viewMeshFilter;
+	Mesh viewMesh;
+
+	void Start() {
+		viewMesh = new Mesh ();
+		viewMesh.name = "View Mesh";
+		viewMeshFilter.mesh = viewMesh;
+
+		StartCoroutine ("FindTargetsWithDelay", .2f);
+	}
+
+
+	IEnumerator FindTargetsWithDelay(float delay) {
+		while (true) {
+			yield return new WaitForSeconds (delay);
+			FindVisibleTargets ();
+		}
+	}
+
+	void LateUpdate() {
+		DrawFieldOfView ();
+	}
+
+	void FindVisibleTargets() {
+		visibleTargets.Clear ();
+		var targetsInViewRadius = Physics.OverlapSphere (transform.position, viewRadius, targetMask);
+
+		for (int i = 0; i < targetsInViewRadius.Length; i++) 
+		{
+			var target = targetsInViewRadius [i].transform;
+			Vector3 dirToTarget = (target.position - transform.position).normalized;
+			if (Vector3.Angle (transform.forward, dirToTarget) < viewAngle / 2) 
+				visibleTargets.Add (target);
+		}
+	}
+
+	void DrawFieldOfView() {
+		int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+		float stepAngleSize = viewAngle / stepCount;
+		var viewPoints = new List<Vector3> ();
+		for (int i = 0; i <= stepCount; i++) {
+			float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
+			Vector3 dir = DirFromAngle (angle, true);
+			viewPoints.Add (transform.position + dir * viewRadius);
+		}
+
+		int vertexCount = viewPoints.Count + 1;
+		Vector3[] vertices = new Vector3[vertexCount];
+		int[] triangles = new int[(vertexCount-2) * 3];
+
+		vertices [0] = Vector3.zero;
+		for (int i = 0; i < vertexCount - 1; i++) {
+			vertices [i + 1] = transform.InverseTransformPoint(viewPoints [i]) + Vector3.forward * maskCutawayDst;
+
+			if (i < vertexCount - 2) {
+				triangles [i * 3] = 0;
+				triangles [i * 3 + 1] = i + 1;
+				triangles [i * 3 + 2] = i + 2;
+			}
+		}
+
+		viewMesh.Clear ();
+
+		viewMesh.vertices = vertices;
+		viewMesh.triangles = triangles;
+		viewMesh.RecalculateNormals ();
+	}
+
+
+	public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
+		if (!angleIsGlobal) {
+			angleInDegrees += transform.eulerAngles.y;
+		}
+		return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad),0,Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+	}
+}
